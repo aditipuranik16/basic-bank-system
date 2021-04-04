58c986427c0d93d0c0e1ceb09fb7675d39f06455
aditipuranik16 <61420553+aditipuranik16@users.noreply.github.com>
2021-04-04 17:09:31 +0530

    Create index.php

---
+++ files/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="CSS/style.css">
+  <link rel="stylesheet" href="CSS/footer.css">
+  <title>SPARK BANK</title>
+</head>
+<body>
+    <!--Main header-->
+    <header class="main-header">
+            <nav class="navbar navbar-expand-sm scrolling-navbar navbar-light fixed-top">
+                <div class="container">
+                    <a href="index.php" class="navbar-brand">
+                        <i class="fas fa-university"></i>BANK</a>
+                    <button class="navbar-toggler" data-toggle="collapse" data-target="#ui-navbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="ui-navbar">
+                        <ul class="navbar-nav ml-auto"> 
+                            <li class="nav-item px-2">
+                                <a class="nav-link" href="usertable.php">VIEW CUSTOMERS</a>
+                            </li>
+                            <li class="nav-item px-2">
+                                <a class="nav-link" href="history.php">TRANSACTION HISTORY</a>
+                            </li>
+                            <li class="nav-item px-2">
+                                <a href="index.php" class="btn btn-danger ">HOME</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        
+        <div class="landing-div">
+            <div class="landing-wrapper d-flex flex-column h-100 align-items-center justify-content-center text-center">
+                <div class="carousel slide carousel-fade" data-ride="carousel">
+                   <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <h1 class="display-4 animated slideInLeft delay-1s">Enjoy SPARK BANK money <br> With Us</h1>
+                        </div>
+                        <div class="carousel-item">
+                            <h1 class="display-4 animated slideInDown delay-1s">Feel free to TRANSFER <br> With Us</h1>
+                        </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+<footer id="foot">
+  <div class="container-fluid p-0">
+    <div class="row text-left">
+      <div class="col-md-5 col-sm-5">
+        <p class="pt-4 text-muted">Copyright ©2021 All rights reserved | This website is made by
+          <span>ADITI S PURANIK - THE SPARKS FOUNDATION</span>
+        </p>
+      </div>
+      </div>
+  </div>
+ </footer> 
+</body>
+</html>
