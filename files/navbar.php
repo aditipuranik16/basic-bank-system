d25047759b77864d5b002e246dea71d6539460f6
aditipuranik16 <61420553+aditipuranik16@users.noreply.github.com>
2021-04-04 17:09:56 +0530

    Add files via upload

---
+++ files/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <i class="fa fa-bars "></i>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto text-right">
+          <li class="nav-item">
+            <a class="nav-link active-home" href="index.php">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="history.php">TRANSACTION HISTORY</a>
+          </li>
+        </ul>
+      </div>
+</nav>
